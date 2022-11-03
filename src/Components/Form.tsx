df98f1953556e03da5e06d120d58a2c7ad9a50b0
Abhishek Bhosale <abhishek-bhosale@pluralsight.com>
2022-11-04 00:18:25 +0530

    updated2

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -1,26 +1,31 @@
 import React, { SyntheticEvent } from "react"
-import PropTypes, { string } from "prop-types"
+import PropTypes, { func, string } from "prop-types"
 
 interface formtype{
     name:String,
     sirname:String,
     maritalStatus:Boolean
 }
+interface funcType{
+    handleClick:()=>void;
+}
 
 
-export const Form =({backgroundColor="red"})=>{
+export const Form =({backgroundColor="red",marginTop="200px",buttonColor="green",paddingTop="20px",paddingLeft="50px"})=>{
 
     let [formdata,setFormdata] = React.useState<formtype>({name:"",sirname:"",maritalStatus:false})
 
     let handleChange=(e:{target: {name:string; value:string}}) => {
         setFormdata({...formdata,[e.target.name]: e.target.value});
+       // onChange=((e)=>setFormdata(e.target.value))
     }
 
     let handleSubmit=(e:SyntheticEvent)=>{
         e.preventDefault() ;
         console.log(formdata)
     }
-    const style={backgroundColor,marginTop}
+    const style={backgroundColor,marginTop,paddingTop,paddingLeft}
+
     return (
         <form style={style}onSubmit={handleSubmit}>
             <div>
@@ -36,14 +41,17 @@ export const Form =({backgroundColor="red"})=>{
                 </div>
                 <input style={{backgroundColor:buttonColor}} type={"submit"}/>
             </div>
-            <button onClick={handleClick}>Clicked</button>
-
+            {/* <button onClick={handleClick}>Clicked</button> */}
         </form>
     )
 }
+
+
 Form.propTypes={
     backgroundColor:PropTypes.string,
     marginTop:PropTypes.string,
     buttonColor:PropTypes.string,
-    handleClick:PropTypes.func
+    paddingTop:PropTypes.string,
+    paddingLeft:PropTypes.string,
+    // handleClick:PropTypes.func
 }
--- src/stories/Form.stories.tsx
+++ src/stories/Form.stories.tsx
@@ -1,45 +1,49 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { Button } from './Button';
+
+
+import  {Form}  from '../Components/Form';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Example/Button',
-  component: Button,
+  title: 'Example/Form',
+  component: Form,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   // argTypes: {
   //   backgroundColor: { control: 'color' },
   // },
-} as ComponentMeta<typeof Button>;
+} as ComponentMeta<typeof Form>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+const Template: ComponentStory<typeof Form> = (args) => <Form {...args} />;
 
 export const Pink = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Pink.args = {
-  primary : false,
-  size : 'medium',
   backgroundColor:"pink",
-  label:"clickme",
+  marginTop:"200px",
+  buttonColor:"red",
+  paddingTop: "20px",
+  paddingLeft: "180px" 
 };
 
 export const Red = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Red.args = {
-  primary : false,
-  size : 'medium',
   backgroundColor:"red",
-  label:"button2",
+  marginTop:"200px",
+  buttonColor:"green",
+  paddingTop: "20px",
+  paddingLeft: "180px" 
+  // handleClick:()=>alert("clicked red")
 };
 
 export const Green = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Green.args = {
-  primary : false,
-  size : 'medium',
   backgroundColor:"green",
-  label:"button3",
+  marginTop:"200px",
+  buttonColor:"pink",
+  paddingTop: "20px",
+  paddingLeft: "180px" 
 };
-
-
